19be79f3e290e9644611713d6aa945b530bce34f
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-02 14:19:19 +0600

    [ADD] start done routing and controller setup config

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config/config.env" });
 
-console.log(process.env.PORT);
-
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port http://localhost:${process.env.PORT}`);
 });
