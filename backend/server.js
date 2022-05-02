9c62a94d3cf893d3b681c8363eb1ae051ae4c07a
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-02 23:12:13 +0600

    [ADD] working without config.env file getting port variable

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config/config.env" });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
 });
