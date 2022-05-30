9ecbf096602ada53db144b3116cb538064916434
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-30 19:44:51 +0600

    [ADD] login system implementation working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,6 @@ const catchAsyncError = require("../middlewares/catchAsyncError");
 const User = require("../models/userModel");
 
 //Register a User
-
 exports.registerUser = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.create({
@@ -18,5 +17,19 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
   res.status(201).json({
     success: true,
     user,
+  });
+});
+
+//Login a User
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.isPasswordMatch(password, user.password))) {
+    return next(new ErrorHandler("Invalid Credentials", 401));
+  }
+  // const token = user.generateToken();
+  res.status(200).json({
+    success: true,
+    token,
   });
 });
