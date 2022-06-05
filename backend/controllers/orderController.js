1ab04e4d97495322eccd48980edcc9c160a653ea
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-05 17:36:23 +0600

    [ADD] update order process or status and delete order is would be possible just for admin route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -58,3 +58,71 @@ exports.myOrders = catchAsyncError(async (req, res, next) => {
     orders,
   });
 });
+
+//get all orders --(Admin)
+exports.getAllOrders = catchAsyncError(async (req, res, next) => {
+  const orders = await Order.find({});
+
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+// update Order Status -- Admin
+exports.updateOrder = catchAsyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  order.orderItems.forEach(async (orderd) => {
+    await updateStock(orderd.product, orderd.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(productId, quantity) {
+  const product = await Product.findById(productId);
+  //stock -1 for each order
+  if (product.Stock === -1) {
+    return;
+  }
+  product.Stock -= quantity;
+  await product.save({ validateBeforeSave: false });
+}
+
+// delete Order -- Admin
+exports.deleteOrder = catchAsyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
