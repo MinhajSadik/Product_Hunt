f4c3fdcfe6bdb039369106eaf04a0e41646447a0
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-06 00:33:05 +0600

    [ADD] working in frontend part header and footer side

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Layout/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
