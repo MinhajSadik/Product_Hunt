b30d0a1612d867422eeef22502021d0a5f829750
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-08 17:59:13 +0600

    [ADD] review and styling for single product in single page

--- frontend/src/Components/Product/ProductDetails.js
+++ frontend/src/Components/Product/ProductDetails.js
@@ -1,14 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+// import {
+//   Dialog,
+//   DialogActions,
+//   DialogContent,
+//   DialogTitle,
+//   Button,
+// } from "@material-ui/core";
 import Carousel from "react-material-ui-carousel";
+import ReactStars from "react-rating-stars-component";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductDetails } from "../../actions/productAction";
 import "./ProductDetails.css";
+import ReviewCard from "./ReviewCard";
 
 const ProductDetails = ({ match }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const dispatch = useDispatch();
+
   const { product, loading, error } = useSelector(
     (state) => state.productDetails
   );
-  const dispatch = useDispatch();
+  const options = {
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
+
+  const addToCartHandler = () => {};
+  const decreaseQuantity = () => {};
+  const increaseQuantity = () => {};
+  const submitReviewToggle = () => {};
 
   useEffect(() => {
     dispatch(getProductDetails(match.params.id));
@@ -29,7 +52,90 @@ const ProductDetails = ({ match }) => {
               ))}
           </Carousel>
         </div>
+        <div className="detailsBlock-1">
+          <h2>{product.name}</h2>
+          <p>Product # {product._id}</p>
+        </div>
+        <div className="detailsBlock-2">
+          <ReactStars {...options} />
+          <span className="detailsBlock-2-span">
+            {" "}
+            ({product.numOfReviews} Reviews)
+          </span>
+        </div>
+        <div className="detailsBlock-3">
+          <h1>{`₹${product.price}`}</h1>
+          <div className="detailsBlock-3-1">
+            <div className="detailsBlock-3-1-1">
+              <button onClick={decreaseQuantity}>-</button>
+              <input readOnly type="number" value={quantity} />
+              <button onClick={increaseQuantity}>+</button>
+            </div>
+            <button
+              disabled={product.Stock < 1 ? true : false}
+              onClick={addToCartHandler}
+            >
+              Add to Cart
+            </button>
+          </div>
+          <p>
+            Status:
+            <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+              {product.Stock < 1 ? "OutOfStock" : "InStock"}
+            </b>
+          </p>
+
+          <div className="detailsBlock-4">
+            Description : <p>{product.description}</p>
+          </div>
+
+          <button onClick={submitReviewToggle} className="submitReview">
+            Submit Review
+          </button>
+        </div>
       </div>
+      <h3 className="reviewsHeading">REVIEWS</h3>
+      {/* <Dialog
+        aria-labelledby="simple-dialog-title"
+        open={open}
+        onClose={submitReviewToggle}
+      >
+        <DialogTitle>Submit Review</DialogTitle>
+        <DialogContent className="submitDialog">
+          <ReactStars
+            onChange={(e) => setRating(e.target.value)}
+            value={rating}
+            size="large"
+          />
+
+          <textarea
+            className="submitDialogTextArea"
+            cols="30"
+            rows="5"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          ></textarea>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={submitReviewToggle} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={reviewSubmitHandler} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog> */}
+
+      {product.reviews && product.reviews[0] ? (
+        <div className="reviews">
+          {product.reviews &&
+            product.reviews.map((review) => (
+              <ReviewCard key={review._id} review={review} />
+            ))}
+        </div>
+      ) : (
+        <p className="noReviews">No Reviews Yet</p>
+      )}
     </>
   );
 };
