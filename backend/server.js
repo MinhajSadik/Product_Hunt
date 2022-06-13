817242fb07131cef164232857d8cb1aad31b648f
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-13 15:30:59 +0600

    [ADD] nothing just start working

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ process.on("uncaughtException", (err) => {
 
 connectDatabase();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
