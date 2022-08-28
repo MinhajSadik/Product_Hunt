908f0fba326e898c0b313eeaffe365dec73cd173
MinhajSadik <MinhajoSadik@gmail.com>
2022-08-28 18:53:29 +0600

    [DONE] all products show in home and product page problem solved

--- frontend/src/redux/api.js
+++ frontend/src/redux/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
+
+
 //get token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
