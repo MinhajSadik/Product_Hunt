fda58597c58ba0c7e813f5ffc646ce60a0e12091
MinhajSadik <MinhajoSadik@gmail.com>
2022-09-21 23:10:03 +0600

    [NOTHING] just push

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,10 +4,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
-  // const token = req.headers.authorization;
   const token = req.headers.authorization.split(" ")[1];
-
-  // console.log(token);
 
   if (!token) {
     return next(new ErrorHandler("Please Login to access this resource", 401));
