9160c2d62e249a6a8acea8b473a3d0dbf56fd6f1
MinhajSadik <MinhajoSadik@gmail.com>
2022-09-26 12:47:14 +0600

    [ERROR] deployment error occured cors origin issue

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
 
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
