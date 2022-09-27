0f8daf21fbd7d526712b94a23c3e6825d3886cc0
MinhajSadik <MinhajoSadik@gmail.com>
2022-09-27 13:27:56 +0600

    [ERROR] deployment error occured cors origin issue

--- backend/app.js
+++ backend/app.js
@@ -23,11 +23,11 @@ app.use("/api/v1/", order);
 
 // if (process.env.NODE_ENV) {
 // console.log(path.resolve(__dirname, "../frontend/build/index.html"));
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname + "../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "../frontend/build/index.html"));
+});
 // }
 
 // error middleware
