ef67967395c3eda9d86c5b8d61619b46340431e3
MinhajSadik <MinhajoSadik@gmail.com>
2022-09-30 18:45:24 +0600

    [CORS] cors origin block issue trying to solve

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
